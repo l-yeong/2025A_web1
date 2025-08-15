1ca4cd104fb0c85e7f8fe8d4e295d2d7adcec713
l-yeong <yahn10@naver.com>
2025-08-15 15:34:37 +0900

    아이디/비밀번호 찾기에서 이름,연락처 일치 하지 않았을때 alert 이벤트 발생 되지 않음, 확인해보기

--- src/main/resources/static/js/member/search.js
+++ src/main/resources/static/js/member/search.js
@@ -1,17 +1,23 @@
 console.log("search.js")
-const search = async()=>{
+const search = async () => {
+    try{
     const mname = document.querySelector('.mname').value;
     const mphone = document.querySelector('.mphone').value;
-    const obj={mname,mphone};
+    const obj = { mname, mphone };
 
-    const response = await fetch("/member/search",option);
-    const data= await response.json();
+    const option = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(obj)
+    }
 
-    if(data==true){
-        alert("수정성공");
-        location.href=`/waiting2/view.jsp?wno=${wno}`;
-    }else{
-        alert("수정실패");
+    const response = await fetch("/member/search", option);
+    const data = await response.json();
+
+    if (data && data.mname && data.mphone) {
+        alert(`아이디:${data.mid} 비밀번호:${data.mpwd}`);
+    } else {
+        alert("입력한 정보가 일치 하지 않습니다.");
     }//if end
-
-}
+    }catch(error){console.log(error)}//catch end
+}//func end
